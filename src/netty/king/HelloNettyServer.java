c478a26c14a1afc0a34bb464ecd46cfb99b36dda
junyang.shi <rennes990@163.com>
2022-05-21 21:05:00 +0800

    Add HelloNetty App

--- src/netty/king/HelloNettyServer.java
+++ src/netty/king/HelloNettyServer.java
@@ -1,9 +1,60 @@
 package netty.king;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
 /**
  * @author junyang.shi
  * @date 2022/05/20 17:25
  **/
 public class HelloNettyServer {
+    private int port;
 
+    /**
+     * @param port
+     */
+    public HelloNettyServer(int port) {
+        this.port = port;
+    }
+
+    public void run() throws Exception {
+        EventLoopGroup boosGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(boosGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new HelloNettyServerHandler());
+                        }
+                    }).option(ChannelOption.SO_BACKLOG, 128)
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+            ChannelFuture f = b.bind(port).sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+            boosGroup.shutdownGracefully();
+        }
+    }
+
+    /**
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+        System.out.println("开始建立Netty服务器...");
+        int port = 8080;
+        if(args.length > 0) {
+            port = Integer.parseInt(args[0]);
+        }
+        new HelloNettyServer(port).run();
+    }
 }
--- src/netty/king/HelloNettyServerHandler.java
+++ src/netty/king/HelloNettyServerHandler.java
@@ -1,8 +1,38 @@
 package netty.king;
 
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.ReferenceCountUtil;
+
 /**
  * @author junyang.shi
  * @date 2022/05/20 17:25
  **/
-public class HelloNettyServerHandler {
+public class HelloNettyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * @param ctx
+     * @param msg
+     */
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) {
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+            ReferenceCountUtil.release(msg);
+        }
+    }
+
+    /**
+     * @param ctx
+     * @param cause
+     */
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();;
+        ctx.close();
+    }
 }
